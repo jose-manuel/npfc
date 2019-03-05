f509f5dd2e4f92a6ab4e0fe63ecc7db896b209ab
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-05 16:23:53 +0100

    build(gitignore): added docs/build

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6-7-gc26d049',
+    VERSION: '0.0.6-8-gf1638d9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.6-6-ga6609ac',
+    version='0.0.6-7-gc26d049',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
