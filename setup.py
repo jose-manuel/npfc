3d580a646518921d06898f51fed13746ea5f4619
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-05 16:50:57 +0100

    feat(bin): added load_mols test

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.6-7-gc26d049',
+    version='0.0.6-8-gf1638d9',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
@@ -30,4 +30,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    scripts=['bin/load_mols'],
 )
