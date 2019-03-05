d5585ca051793cac4bc5dc6e2a41f066b46e33e1
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-05 18:00:53 +0100

    feat(bin): load_mols now works with csv files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.6-12-g7fe9512',
+    version='0.0.6-13-g925a7f1',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
