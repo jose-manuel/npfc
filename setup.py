51a9e65133ce6e2c8c4a317ef14747bb3263157a
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-06 10:00:22 +0100

    feat(bin): load_mols can now either encode or decode mols

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.6-15-gd5585ca',
+    version='0.0.6-16-g90ed6e3',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
