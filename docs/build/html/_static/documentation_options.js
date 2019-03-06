9e9e30bf7526ff4e126148b8b834422f40dba3af
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-06 09:52:14 +0100

    docs(bin): better description for load_mols

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6-15-gd5585ca',
+    VERSION: '0.0.6-16-g90ed6e3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.6-14-gf1db24d',
+    version='0.0.6-15-gd5585ca',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
