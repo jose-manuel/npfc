330315209b6ef12011a0655ba28925123e174294
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-11 15:25:44 +0100

    refactor(save): tests adaptated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.9-1-gc7bdaf2',
+    version='0.0.9-2-g7f00d87',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
