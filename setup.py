2c669916ff8e13e73a234eed0f40469397ce9704
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-26 13:40:15 +0100

    test(pytest.ini): warnings are now hidden when running pytest

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.12',
+    version='0.0.13',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
