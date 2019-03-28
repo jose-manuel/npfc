b5b29f73a7dd740ed1e6f3f11c6eca6f7d024710
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-28 17:37:06 +0100

    fix(fragment): classify_frag_comb had a typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.0.13-7-g097fd80',
+    version='0.0.14',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
