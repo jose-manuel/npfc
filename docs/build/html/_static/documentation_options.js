ba2f9bfa18cf71c9f20366d72a09a6bd52ced4e7
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-03-28 17:38:30 +0100

    fix(fragment): classify_frag_comb had another typo

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.14',
+    VERSION: '0.0.14-1-gb5b29f7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
