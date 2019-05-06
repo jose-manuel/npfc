7305c96ee51e5992a66dc29f3b2ecebe09609996
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-05-06 13:37:45 +0200

    test(fragment): cleaned tests

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0-3-ge2ec8ef',
+    VERSION: '0.1.0-4-g8ac33af',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
