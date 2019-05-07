3202aca496136865694e87d5ad58ea395c31b05e
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-05-07 16:23:37 +0200

    feat(bin): standardize_mols now has an option for computing 2D coordinates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.1.1',
+    version='0.1.1-1-g2e46296',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
