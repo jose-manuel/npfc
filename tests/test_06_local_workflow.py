abaf32b839cde9e2d578d966c3aaad6ced5f521b
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-05-28 11:36:12 +0200

    test(local_workflow): snakemake local_worklow is now incorpored to the pytest suite

--- tests/test_06_local_workflow.py
+++ tests/test_06_local_workflow.py
@@ -6,15 +6,22 @@ for a local run of the npfc workflow.
 """
 # standard library
 import logging
-# data handling
-import pandas as pd
-# chemoinformatics
-from rdkit import Chem
-# tests
-import pytest
-from npfc import fragment
-
+import subprocess
+from pathlib import Path
 # debug
 # logging.basicConfig(level=logging.DEBUG)
 
-## TO DO LATER
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+def test_init_folder():
+    """Removes all files in tmp worklow output folder"""
+    for f in [x for x in Path('tests/tmp/local_workflow').iterdir() if x.is_file()]:
+        f.unlink()
+
+
+def test_run_local_workflow():
+    """Run the local workflow defined in tests/data"""
+    subprocess.run("snakemake -s tests/local_workflow.smk", shell=True, check=True)
+    assert Path("tests/tmp/local_workflow/local_workflow.png").exists()
+    assert Path("tests/tmp/local_workflow/data/chembl_small_001_map_crms.csv.gz").exists()
