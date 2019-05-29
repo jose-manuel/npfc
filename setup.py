80db5b964644d08c1ec0ff6ba4f126df9e1c0abc
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-05-29 15:45:56 +0200

    fix(bin/deglyco_mols): updated help message, removed a few outdated variables

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.1.8-2-gabaf32b',
+    version='0.1.9',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for classifying fragment combinations in molecules",
