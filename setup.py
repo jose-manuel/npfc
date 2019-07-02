8b7585a9a3515ca7784cb2967962c747b0c71b0e
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-07-02 09:26:41 +0200

    feat(bin/plot_pipeline_results): Added % of fm/mols to _map_perc_frag_cov counts

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.3.0',
+    version='0.3.1',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
