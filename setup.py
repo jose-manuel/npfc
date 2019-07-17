50128cdef702ada40522f574758ade3aa85a5d61
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-07-17 13:43:08 +0200

    docs(index): added an image

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.3.5-10-gf8d9d0e',
+    version='0.3.5-11-g2917511',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
