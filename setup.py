945c5fe9f2f852af10af426eb52ffd35cb61d635
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-10-08 15:37:43 +0200

    fix: 🐛 typo in run_protocol

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.5.0-1-gc33290d',
+    version='0.5.1',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
