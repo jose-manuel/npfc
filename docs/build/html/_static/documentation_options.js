84fe85bf687c88ed6e39d25911a07e11290b8935
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-11-18 09:33:13 +0100

    fix: 🐛 results_analyze now with pnp barplot and fixed figsize

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.6-3-ga324b9b',
+    VERSION: '0.5.6-4-g5cbd927',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.5.6-2-g73c0fe5',
+    version='0.5.6-3-ga324b9b',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
