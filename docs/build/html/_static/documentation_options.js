291921b958b6284d0b39526af3eb1b36686c3865
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-11-18 11:05:18 +0100

    fix: 🐛 typo

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.7-3-g5639602',
+    VERSION: '0.5.7-4-g9b14212',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
