1f35dbe631e209e809d1a0104442d632e55c3107
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2019-12-09 17:20:27 +0100

    feat: 🎸 report_protocol: plots for fc/fcc

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.9-1-gb9a41a1',
+    VERSION: '0.5.9-2-g2f6e5fe',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.5.9',
+    version='0.5.9-1-gb9a41a1',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
