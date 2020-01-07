19921177a902b7b0ae2b34baaad3e4e40b5ad0a6
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-01-07 11:49:48 +0100

    perf: ⚡️ report_protocol: decreased memory usage in fs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.5.9-2-g2f6e5fe',
+    version='0.5.10',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
