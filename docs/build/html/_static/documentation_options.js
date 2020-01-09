963a70854e8d11a79f2625ac5cf58f512fe543a6
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-01-09 10:37:18 +0100

    style: 💄 report_protocol: decrease fig width

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.10-1-g1992117',
+    VERSION: '0.5.10-2-g8bc99d2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
