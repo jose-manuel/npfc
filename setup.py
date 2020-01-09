b2cb5d8dc2d3ca88806a63351d9326a68e3cc258
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-01-09 11:24:25 +0100

    style: 💄 report_protocol: decreased width for prep plots

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.5.10-1-g1992117',
+    version='0.5.10-2-g8bc99d2',
     author="Stephane Bourg, Jose-Manuel Gally",
     author_email="stephane.bourg@crns.fr",
     description="A package for classifying fragment combinations in molecules",
