f2c004aa23aebcc70f4cd4cb456a071f2075acee
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-01-12 22:34:06 +0100

    fix: 🐛 act_preprocess

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.11',
+    VERSION: '0.5.11-1-g9e15a26',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
