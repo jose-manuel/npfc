379099d2517f39117574e31b48264c7d04d5dc46
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-02-26 14:21:50 +0100

    fix: 🐛 report_protocol: fixed a lineplot

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.16-20-gb0fd500',
+    VERSION: '0.5.16-21-g0ebe95c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
