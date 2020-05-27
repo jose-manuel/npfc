740e56d17911cdb9d565fd5541e11814ab2ecfc1
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-05-27 08:56:28 +0200

    fix: 🐛 bin/fcg_annotate_pnp: wrong data used for natref
    Added options for filtering input fragment combinations during fcg
    generation too

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.6.3-7-ge5aab23',
+    version='0.6.4',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for analyzing fragment combinations in natural and synthetic molecules",
