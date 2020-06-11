d1060b1e26731bcb27f7263759146aafdb7f2638
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-06-11 16:15:06 +0200

    fix: 🐛 bin/report_protocol: adapt code for new fcg columns

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.8-1-gad2fed1',
+    VERSION: '0.6.8-2-g430c566',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
