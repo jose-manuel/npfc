1a45225ab14b054a3d91106d6f08d75a628e8449
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-11-09 00:23:11 +0100

    style: 💄 bin/fct_fragment_fragment: removed code artifacts

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.7.0-4-gfae5da1',
+    version='0.7.0-5-g1924243',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for analyzing fragment combinations in natural and synthetic molecules",
