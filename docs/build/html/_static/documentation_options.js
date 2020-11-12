fcf4110bbbf636a823788f081f42d1b6d7759fa8
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2020-11-12 22:49:51 +0100

    fix: 🐛 smk: typo for fcp log dir for fragments

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.3',
+    VERSION: '0.7.3-1-gb0d9e5a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
