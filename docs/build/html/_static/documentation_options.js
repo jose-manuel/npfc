c8d6e2e097ec0b6451eee86f9f3bee05f39a42cd
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2021-01-14 10:50:24 +0100

    feat: 🎸 data/report.mplstyle: centralized file with mpl/sns opt

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.6-8-g747498d',
+    VERSION: '0.7.6-9-g66ad263',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.7.6-7-gbf6e832',
+    version='0.7.6-8-g747498d',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for analyzing fragment combinations in natural and synthetic molecules",
