6459e9d6056200fbd41809d65ab46be6032b5faa
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2021-01-15 15:49:05 +0100

    fix: 🐛 bin/run_protocol_fc-fct: fix smk command print

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.7.6-12-gc19b063',
+    version='0.7.6-13-g6865211',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for analyzing fragment combinations in natural and synthetic molecules",
