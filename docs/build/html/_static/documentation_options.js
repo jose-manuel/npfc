966c14f4d4356850788fb4610d7f57c840f1f043
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2021-02-02 09:40:49 +0100

    docs: ✏️ bin/fc/report/count_mols: fixed docstrings

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.8-1-g53cc555',
+    VERSION: '0.7.8-2-gbdde60b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
