4d3ee035260386ce07757abdefe0f89f49daa184
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2021-02-08 17:45:41 +0100

    feat: 🎸 bin/fc/mols_dedupl: waiting time, csv ref hdf->csv
    There are way too many problems with the deduplication. I am concerned
    about that HDF file, so I switch o csv format. To avoid that all chunks
    try to access to the reference file at once, I added a waiting time at
    start up.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="npfc",
-    version='0.7.8-4-g690b656',
+    version='0.7.8-5-g473ee36',
     author="Jose-Manuel Gally",
     author_email="josemanuel.gally@mpi-dortmund.mpg.de",
     description="A package for analyzing fragment combinations in natural and synthetic molecules",
