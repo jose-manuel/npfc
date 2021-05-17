7080c9d452983479b565894ec0cbfc4871f721a0
Jose-Manuel Gally <josemanuel.gally@mpi-dortmund.mpg.de>
2021-05-17 17:25:20 +0200

    docs: ✏️ index: added entry for notebook + sorted index

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.12-1-g54bfefc',
+    VERSION: '0.7.12-2-gc51ccec',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
